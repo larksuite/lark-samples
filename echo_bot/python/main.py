d69733cfc989b394b89cd1b1d2618a5de6a1c2bc
wefong-hub <wefong@bytedance.com>
2025-09-22 15:54:09 +0800

    Update main.py

--- echo_bot/python/main.py
+++ echo_bot/python/main.py
@@ -17,7 +17,7 @@ def do_p2_im_message_receive_v1(data: P2ImMessageReceiveV1) -> None:
         {
             "text": "收到你发送的消息："
             + res_content
-            + "\nReceived messages_1:"
+            + "\nReceived messages_2:"
             + res_content
         }
     )
